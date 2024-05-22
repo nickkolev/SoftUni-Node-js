caa5f1305291d4caf13b8bc1f7c277fc8555131e
Nikola Kolev <nickkolev06@abv.bg>
2024-05-22 19:37:36 +0300

    .

--- CatShelter2/src/handlers/home.js
+++ CatShelter2/src/handlers/home.js
@@ -1,11 +1,30 @@
-const cats = require("../../data/cats.json");
-const homeTemplate = require('../../views/home/index.html.js');
+const { getCats } = require('../model');
+const { layout, readTemplate } = require('../util');
 
-function homeHandler(req, res) {
+function catFragment(cat) {
+    return`
+    <li>
+        <img src="${cat.imageUrl}">
+        <h3>${cat.name}</h3>
+        <p><span>Breed: </span>${cat.breed}</p>
+        <p><span>Description: </span>${cat.description}</p>
+        <ul class="buttons">
+            <li class="btn edit"><a href="">Change Info</a></li>
+            <li class="btn delete"><a href="">New Home</a></li>
+        </ul>
+    </li>`;
+}
+
+async function homeHandler(req, res) {
+    const template = await readTemplate('home/index');
     res.writeHead(200, [
         'Content-Type', 'text/html'
     ]);
-    res.write(homeTemplate(cats));
+
+    const cats = await getCats();
+    const html = template.replace('%%catContent%%', cats.map(catFragment).join('\n'));
+
+    res.write(await layout(html, true));
     res.end();
 }
 
--- CatShelter2/src/index.js
+++ CatShelter2/src/index.js
@@ -1,27 +1,40 @@
 const http = require('http');
-const port = 3000;
+
+const { staticFileHandler } = require('./handlers/static');
 const { homeHandler } = require('./handlers/home');
-const { stylesHandler } = require('./handlers/styles');
-const { catsHandler } = require('./handlers/cat');
+const { addBreedHandler, postBreedHandler } = require('./handlers/addBreed');
+const { addCatHandler } = require('./handlers/addCat');
 
 const routes = {
-    '/': homeHandler,
-    '/home': homeHandler,
-    '/cats/add-cat': catsHandler,
-    '/cats/add-breed': catsHandler,
+    'GET': {
+        '/': homeHandler,
+        '/index.html': homeHandler,
+        '/cats/add-breed': addBreedHandler,
+        '/cats/add-cat': addCatHandler
+    },
+    'POST': {
+        '/cats/add-breed': postBreedHandler
+    }
 };
 
 http.createServer((req, res) => {
-    const route = routes[req.url];
-    if (typeof route === 'function') {
-        route(req, res);
-    } else if (req.url.endsWith('.css')) {
-        stylesHandler(req, res);
-    } else {
-        res.writeHead(404, {
-            'Content-Type': 'text/plain'
-        });
-        res.write('404 Not Found');
-        res.end();
+    const methodRoutes = routes[req.method];
+
+    if (methodRoutes) {
+        const route = methodRoutes[req.url];
+        if (typeof route == 'function') {
+            route(req, res);
+            return;
+        }
     }
-}).listen(port);
+
+    if (staticFileHandler(req, res)) {
+        return;
+    }
+
+    res.writeHead(404, [
+        'Content-Type', 'text/plain'
+    ]);
+    res.write('404 Not Found');
+    res.end();
+}).listen(3000);
