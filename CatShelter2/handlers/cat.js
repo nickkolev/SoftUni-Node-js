4a3be7bbf1280a982d9fa765ac575c0532311feb
Nikola Kolev <nickkolev06@abv.bg>
2024-05-22 22:45:39 +0300

    adding breeds

--- CatShelter2/handlers/cat.js
+++ CatShelter2/handlers/cat.js
@@ -45,6 +45,75 @@ module.exports = (req, res) => {
         index.on('error', (err) => {
             console.log(err);
         });
+    } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
+        let formData = '';
+
+        req.on('data', (data) => {
+            formData += data;
+        });
+
+        req.on('end', () => {
+            let body = qs.parse(formData);
+
+            console.log(body);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                let breeds = JSON.parse(data);
+                breeds.push(body.breed);
+
+                let json = JSON.stringify(breeds);
+
+                fs.writeFile('./data/breeds.json', json, 'utf-8', () => console.log('The breed was uploaded successfully!'));
+            });
+        });
+
+        res.writeHead(301, {
+            location: '/'
+        });
+
+        res.end();
+    } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
+        let form = new formidable.IncomingForm();
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                throw err;
+            }
+
+            let oldPath = files.upload.path;
+            let newPath = path.normalize(path.join(__dirname, '../content/images/' + files.upload.name));
+
+            fs.rename(oldPath, newPath, (err) => {
+                if (err) {
+                    throw err;
+                }
+
+                console.log('Files was uploaded successfully!');
+            });
+
+            fs.readFile('./data/cats.json', 'utf-8', (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                let allCats = JSON.parse(data);
+                allCats.push({ id: cats.length + 1, ...fields, image: files.upload.name });
+
+                let json = JSON.stringify(allCats);
+
+                fs.writeFile('./data/cats.json', json, () => console.log('The cat was uploaded successfully!'));
+            });
+        });
+
+        res.writeHead(301, {
+            location: '/'
+        });
+
+        res.end();
     } else {
         return true;
     }
