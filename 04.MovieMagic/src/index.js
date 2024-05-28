c0c458ab5ea9dc8f4e8816ad0b707915789e0b7d
Nikola Kolev <nickkolev06@abv.bg>
2024-05-28 23:27:30 +0300

    setup complete

---
+++ 04.MovieMagic/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("src/public"));
+
+app.get("/", (req, res) => {
+    res.render("home", { layout: false });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
