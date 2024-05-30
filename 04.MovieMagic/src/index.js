db504c149f3a1883a54b3b21139291c34572dc5b
Nikola Kolev <nickkolev06@abv.bg>
2024-05-30 22:39:04 +0300

    .

--- 04.MovieMagic/src/index.js
+++ 04.MovieMagic/src/index.js
@@ -14,7 +14,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static("src/public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("home", { layout: false });
