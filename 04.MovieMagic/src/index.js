e52171e42e25d7ffe4d5a3cdfe5e90d5dac2c43c
Nikola Kolev <nickkolev06@abv.bg>
2024-06-01 10:47:50 +0300

    routes

--- 04.MovieMagic/src/index.js
+++ 04.MovieMagic/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
 
+const routes = require("./routes");
+
 const app = express();
 const port = 3000;
 
@@ -16,10 +18,8 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.render("home", { layout: false });
-});
+app.use(routes);
 
-app.listen(port, () => {
+app.listen(port, () => { 
     console.log(`Server is running on port ${port}`);
 });
