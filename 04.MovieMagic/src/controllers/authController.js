8cb4a4dcb53a414da724d79e0f4646a3828c0505
Nikola Kolev <nickkolev06@abv.bg>
2024-06-08 23:40:16 +0300

    register implemented, working on validating password and rePassword

---
+++ 04.MovieMagic/src/controllers/authController.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+const userService = require("../services/authService");
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    const user = await userService.register(userData);
+
+    res.redirect('/auth/login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('login', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await userService.login(email, password);
+
+    res.redirect('/');
+}); 
+
+module.exports = router;
