9393f44720f2b385083ca395b494213c5d6c8ea3
Nikola Kolev <nickkolev06@abv.bg>
2024-06-15 18:32:52 +0300

    skelet for exam

---
+++ 05.Skeleton/src/controllers/authController.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+
+const authService = require('../services/authService');
+const { getErrorMessage } = require('../utils/errorUtils');
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await authService.register(userData);
+
+        res.cookie('auth', token);
+        res.redirect('/');
+    } catch(err) {
+        res.render('auth/register', {error: getErrorMessage(err)});
+    }
+
+}); 
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+
+        res.cookie('auth', token);
+        res.redirect('/');
+    } catch(err) {
+        res.render('auth/login', {error: getErrorMessage(err)});
+    }
+
+}); 
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+
+    res.redirect('/');
+});
+
+module.exports = router;
---
+++ 05.Skeleton/src/services/authService.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+
+const User = require('../models/User');
+const { SECRET } = require("../config/config");
+
+exports.register = async (userData) => {
+    const user = await User.findOne({ email: userData.email });
+
+    if(user) {
+        throw new Error("User already exists!");
+    }
+
+    if(userData.password !== userData.rePassword) {
+        throw new Error("Passwords do not match!");
+    }
+
+    const createdUser = await User.create(userData);
+
+    const token = await generateToken(createdUser);
+
+    return token;
+};
+
+exports.login = async (email, password) => {
+    // Get user from db
+    const user = await User.findOne({ email });
+    
+    // Check if user exists
+    if(!user) {
+        throw new Error("Email or password incorrect!");
+    }
+    
+    // Check if password is correct
+    let isValid = await bcrypt.compare(password, user.password);
+    if(!isValid) {
+        throw new Error("Email or password incorrect!");
+    }
+
+    const token = await generateToken(user);
+
+    return token;
+};
+
+async function generateToken(user) {
+    // Generate jwt token
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+    };
+    const token = await jwt.sign(payload, SECRET, { expiresIn: "2h" });
+
+    // Return token
+    return token;
+}
